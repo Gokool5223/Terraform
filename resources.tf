01af0b0e1531c1c3639e900fe6a6db135be2c7c6
Gokool <gokulpaithane83@gmail.com>
2022-01-20 22:05:22 +0530

    changes in var structure4

--- resources.tf
+++ resources.tf
@@ -70,7 +70,7 @@ resource "aws_eip" "nat_eip" {
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = [aws_subnet.public_subnet.id]
-  depends_on    = aws_internet_gateway.igw.id
+  depends_on    = [aws_internet_gateway.igw.id]
   tags = {
     Name        = "nat"
     
@@ -143,7 +143,7 @@ resource "aws_security_group" "default-sg" {
   name        = "default-sg"
   description = "Default security group to allow inbound/outbound from the VPC"
   vpc_id      = [aws_vpc.vpc.id]
-  depends_on  = aws_vpc.vpc.id
+  depends_on  = [aws_vpc.vpc.id]
   
   ingress {
     from_port = "0"
