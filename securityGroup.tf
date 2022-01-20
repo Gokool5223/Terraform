633cc64d07abfb3592e9fdc2e9ea830eb950ec04
Gokool <gokulpaithane83@gmail.com>
2022-01-20 22:24:42 +0530

    changes in var structure4

--- securityGroup.tf
+++ securityGroup.tf
@@ -1,24 +1,24 @@
-resource "aws_security_group" "terraform" {
+# resource "aws_security_group" "terraform" {
 
-    name = "Terraform Security"
-    description = "Allow Traffic"
-    vpc_id = aws_vpc.main.id
+#     name = "Terraform Security"
+#     description = "Allow Traffic"
+#     vpc_id = aws_vpc.main.id
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "${var.ssh_port}"
-        cidr_blocks = "0.0.0.0/0"
-    }
+#     ingress {
+#         from_port = 22
+#         to_port = 22
+#         protocol = "${var.ssh_port}"
+#         cidr_blocks = "0.0.0.0/0"
+#     }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = "0.0.0.0/0"
-    }
+#     egress {
+#         from_port = 0
+#         to_port = 0
+#         protocol = "-1"
+#         cidr_blocks = "0.0.0.0/0"
+#     }
 
-    tags = "Terraform-SG"
+#     tags = "Terraform-SG"
 
   
-}
+# }
